e9ce31054e6a56e467ef1a22712d71a1f9bb2d31
Céline <celine.merand@reseau.eseo.fr>
2018-12-20 09:50:42 +0100

    WIP : DatePicker & Calendar

---
+++ Calendar/src/components/Datepicker.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="datepicker__container">
+    <input type="text" :value="date_formatted">
+    <input type="text" :name="name" :value="date_raw"/>
+    <datepickerAgenda v-bind:date="date" name="agenda"></datepickerAgenda>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment';
+  import DatepickerAgendaComponent from './Datepicker-agenda';
+
+  moment.locale('fr');
+
+  export default {
+    components: {
+      datepickerAgenda: DatepickerAgendaComponent
+    },
+    props: {
+      value: {type: String, required: true},
+      format: {type: String, default: 'YYYY-MM-DD'},
+      name: {type: String}
+    },
+    data: function () {
+      return {
+        date: moment(this.value, 'YYYY-MM-DD')
+      }
+    },
+    computed: {
+      date_formatted: function () {
+        return this.date.format(this.format);
+      },
+      date_raw: function () {
+        return this.date.format('YYYY-MM-DD');
+      }
+    }
+  }
+</script>
+
+<style>
+  .datepicker__container {
+    position: relative;
+  }
+</style>
+
